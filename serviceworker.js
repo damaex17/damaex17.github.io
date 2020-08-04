8e80f858f769b92f65179c5af03eb1dc08eeb5a2
Maximilian Weber <maximilian.weber@gmail.com>
2020-08-04 08:01:10 +0000

    fixing serviceworker

--- serviceworker.js
+++ serviceworker.js
@@ -3,7 +3,6 @@ const CACHE = "pwabuilder-offline";
 
 const offlineFallbackPage = "index.html";
 
-// Install stage sets up the index page (home page) in the cache and opens a new cache
 self.addEventListener("install", function (event) {
   console.log("Install Event processing");
 
@@ -20,7 +19,6 @@ self.addEventListener("install", function (event) {
   );
 });
 
-// If any fetch fails, it will look for the request in the cache and serve it from there first
 self.addEventListener("fetch", function (event) {
   if (event.request.method !== "GET") return;
 
@@ -29,7 +27,6 @@ self.addEventListener("fetch", function (event) {
       .then(function (response) {
         console.log("Add page to offline cache: " + response.url);
 
-        // If request was success, add or update it in the cache
         event.waitUntil(updateCache(event.request, response.clone()));
 
         return response;
@@ -42,9 +39,6 @@ self.addEventListener("fetch", function (event) {
 });
 
 function fromCache(request) {
-  // Check to see if you have it in the cache
-  // Return response
-  // If not in the cache, then return error page
   return caches.open(CACHE).then(function (cache) {
     return cache.match(request).then(function (matching) {
       if (!matching || matching.status === 404) {
