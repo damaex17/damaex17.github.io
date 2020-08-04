093fde175cfb775bd2392538fb7f276575a7af18
Maximilian Weber <maximilian.weber@gmail.com>
2020-08-04 08:37:38 +0000

    adding more log messages for service worker

--- loadworker.js
+++ loadworker.js
@@ -1,8 +1,8 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/serviceworker.js', { scope: '/' })
-      .then(() => console.log('service worker registered'))
-      .catch(() => console.error('service worker registration failed'));
+      .then(() => console.log('service worker registered 🙂'))
+      .catch(() => console.error('service worker registration failed 🙄'));
   });
 }
 
